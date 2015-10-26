1a51602ebbf2d592e85632b5da58c1963c8d41fd
Pawel Weglewski <pawel.weglewski@getresponse.com>
2015-10-26 02:52:43 +0100

    Initial project commit

---
+++ WeatherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeatherApp
+//
+//  Created by Pawel Weglewski on 26/10/15.
+//  Copyright © 2015 Pawel Weglewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
